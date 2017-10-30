7a3a42f1510838aaea7f65b1c3e5573e00490804
bentonwong <bentonwong@gmail.com>
2017-10-30 10:34:13 -0700

    stub out explore controller

---
+++ app/controllers/explore_controller.rb
@@ -0,0 +1,26 @@
+class ExploreController < ApplicationController
+
+  def search
+
+  end
+
+  def strava_search
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['client_id'] = ''
+        req.params['client_secret'] = ''
+        req.params['q'] = params[:query]
+      end
+      body = JSON.parse(@resp.body)
+       if @resp.success?
+         @repos = body["items"]
+       else
+         @error = body["meta"]["errorDetail"]
+       end
+       rescue Faraday::TimeoutError
+        @error = "There was a timeout. Please try again."
+       end
+      render 'search'
+    end
+
+end
